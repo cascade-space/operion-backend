4b2ac87468cc1c3cee067f2477e1dbc1d9ee6e9a
siva0704 <vaibhavabg1234@gmail.com>
2025-11-03 01:15:43 +0530

    conflict local

--- tsconfig-paths-bootstrap.js
+++ tsconfig-paths-bootstrap.js
@@ -4,36 +4,20 @@
  */
 const tsConfigPaths = require('tsconfig-paths');
 const path = require('path');
-const fs = require('fs');
 
-// Read and parse tsconfig.json (supports comments)
-function readTsConfig(filePath) {
-  const content = fs.readFileSync(filePath, 'utf8');
-  // Strip single-line comments (// ...)
-  const withoutComments = content.replace(/\/\/.*$/gm, '');
-  // Strip multi-line comments (/* ... */)
-  const cleaned = withoutComments.replace(/\/\*[\s\S]*?\*\//g, '');
-  return JSON.parse(cleaned);
-}
-
-// Read the base tsconfig.json
-const tsConfig = readTsConfig(path.resolve(__dirname, 'tsconfig.json'));
-
-// Map paths from src/ to dist/ for runtime
-// baseUrl is "./src", so we change it to "./dist" for runtime
+// Hardcode the path mappings based on tsconfig.json
+// These paths are mapped from src/ to dist/ at runtime
 const baseUrl = path.resolve(__dirname, './dist');
-const paths = {};
-
-// Map all @/* paths from src/ to dist/
-Object.keys(tsConfig.compilerOptions.paths).forEach(key => {
-  const pathValues = tsConfig.compilerOptions.paths[key];
-  // Replace "./" with "./dist/" to point to compiled files
-  paths[key] = pathValues.map(p => {
-    // If path starts with "./", replace with "./dist/"
-    // e.g., "./middleware/*" -> "./dist/middleware/*"
-    return p.replace(/^\.\//, './dist/');
-  });
-});
+const paths = {
+  '@/*': ['./*'],
+  '@/types/*': ['./types/*'],
+  '@/models/*': ['./models/*'],
+  '@/controllers/*': ['./controllers/*'],
+  '@/middleware/*': ['./middleware/*'],
+  '@/services/*': ['./services/*'],
+  '@/utils/*': ['./utils/*'],
+  '@/config/*': ['./config/*']
+};
 
 // Register the paths
 tsConfigPaths.register({
